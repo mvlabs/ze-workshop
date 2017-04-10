81ab7dabebc20d62ab36e036960f85d959de87dc
Marco Perone <m.perone@mvlabs.it>
2017-04-10 07:47:05 +0200

    user entity

---
+++ app/src/App/Domain/Entity/User.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\App\Domain\Entity;
+
+use App\App\Domain\Value\UserId;
+
+final class User
+{
+    /**
+     * @var UserId
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $surname;
+
+    /**
+     * @var bool
+     */
+    private $administrator;
+
+    private function __construct(
+        UserId $id,
+        string $name,
+        string $surname
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->administrator = false;
+    }
+
+    public static function new(string $name, string $surname): self
+    {
+        $id = UserId::new();
+
+        return new self($id, $name, $surname, false);
+    }
+
+    public static function newAdministrator(string $name, string $surname): self
+    {
+        $id = UserId::new();
+
+        return new self($id, $name, $surname, true);
+    }
+}
---
+++ app/src/App/Domain/Value/UserId.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\App\Domain\Value;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+final class UserId
+{
+    /**
+     * @var UuidInterface
+     */
+    private $id;
+
+    private function __construct(UuidInterface $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function new(): self
+    {
+        return new self(Uuid::uuid4());
+    }
+
+    public static function fromString(string $userId): self
+    {
+        return new self(Uuid::fromString($userId));
+    }
+
+    public function __toString(): string
+    {
+        return $this->id->toString();
+    }
+}
