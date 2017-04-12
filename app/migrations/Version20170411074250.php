c72072e7b9e502f22dba91820eb27240e65989c0
Marco Perone <m.perone@mvlabs.it>
2017-04-12 08:32:13 +0200

    users table migration

---
+++ app/migrations/Version20170411074250.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170411074250 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $userTable = $schema->createTable('users');
+
+        $userTable->addColumn('id', 'string', ['fixed' => true, 'length' => 36, 'notnull' => true]);
+        $userTable->addColumn('name', 'string', ['length' => 1023, 'notnull' => true]);
+        $userTable->addColumn('surname', 'string', ['length' => 1023, 'notnull' => true]);
+
+        $userTable->setPrimaryKey(['id']);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('users');
+    }
+}
