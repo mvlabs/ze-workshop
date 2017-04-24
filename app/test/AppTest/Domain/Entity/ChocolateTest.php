86b0c04c5de93f8d6668a4ba110e3cd3eef29e9a
Marco Perone <m.perone@mvlabs.it>
2017-04-24 09:51:58 +0200

    test approve chocolate

--- app/test/AppTest/Domain/Entity/ChocolateTest.php
+++ app/test/AppTest/Domain/Entity/ChocolateTest.php
@@ -9,9 +9,12 @@ use App\Domain\Entity\User;
 use App\Domain\Value\Address;
 use App\Domain\Value\ChocolateId;
 use App\Domain\Value\Country;
+use App\Domain\Value\Exception\InvalidStatusTransitionException;
+use App\Domain\Value\Exception\UnauthorizedUserException;
 use App\Domain\Value\Percentage;
 use App\Domain\Value\Producer;
 use App\Domain\Value\Quantity;
+use App\Domain\Value\Status;
 use App\Domain\Value\UserId;
 use App\Domain\Value\WrapperType;
 use PHPUnit\Framework\TestCase;
@@ -116,5 +119,112 @@ final class ChocolateTest extends TestCase
         self::assertSame($wrapperType, $chocolate->wrapperType());
         self::assertSame($quantity, $chocolate->quantity());
         self::assertSame($user->id(), $chocolate->lastTransitionUserId());
+    }
+
+    public function testApprove()
+    {
+        $chocolate = Chocolate::submit(
+            ChocolateId::new(),
+            Producer::fromNameAndAddress(
+                'name',
+                Address::fromStreetNumberZipCodeCityRegionCountry(
+                    'street',
+                    '1A',
+                    'AB123',
+                    'Treviso',
+                    'TV',
+                    Country::fromStringCode('it')
+                )
+            ),
+            'bittersweet symphony',
+            Percentage::integer(73),
+            WrapperType::get('box'),
+            Quantity::grams(100),
+            User::new('gigi', 'Zucon')
+        );
+
+        $approver = User::newAdministrator('toni', 'Folpet');
+        $chocolate->approve($approver);
+
+        self::assertSame(Status::APPROVED, $chocolate->status()->getValue());
+        self::assertSame($approver->id(), $chocolate->lastTransitionUserId());
+    }
+
+    public function testApproveNotSubmittedChocolate()
+    {
+        $this->expectException(InvalidStatusTransitionException::class);
+        $this->expectExceptionMessage('You can approve only a chocolate in submitted status');
+
+        $userId = (string) UserId::new();
+        $history = [
+            [
+                'status' => 'submitted',
+                'user_id' => $userId,
+                'user_name' => 'Gigi',
+                'user_surname' => 'Zucon',
+                'user_is_administrator' => true,
+                'date_time' => date_create_immutable()
+            ],
+            [
+                'status' => 'deleted',
+                'user_id' => $userId,
+                'user_name' => 'Gigi',
+                'user_surname' => 'Zucon',
+                'user_is_administrator' => true,
+                'date_time' => date_create_immutable()
+            ]
+        ];
+
+        $chocolate = Chocolate::fromNativeData(
+            (string) ChocolateId::new(),
+            'name',
+            'street',
+            '1A',
+            'AB123',
+            'Treviso',
+            'TV',
+            'it',
+            'bittersweet symphony',
+            73,
+            'box',
+            100,
+            $history
+        );
+
+        $approver = User::newAdministrator('toni', 'Folpet');
+        $chocolate->approve($approver);
+    }
+
+    public function testApproveByNotAdministrator()
+    {
+        $this->expectException(UnauthorizedUserException::class);
+        $this->expectExceptionMessage(sprintf(
+            'The user %s %s is not authorized to approve a submitted chocolate',
+            'toni',
+            'Folpet'
+        ));
+
+        $chocolate = Chocolate::submit(
+            ChocolateId::new(),
+            Producer::fromNameAndAddress(
+                'name',
+                Address::fromStreetNumberZipCodeCityRegionCountry(
+                    'street',
+                    '1A',
+                    'AB123',
+                    'Treviso',
+                    'TV',
+                    Country::fromStringCode('it')
+                )
+            ),
+            'bittersweet symphony',
+            Percentage::integer(73),
+            WrapperType::get('box'),
+            Quantity::grams(100),
+            User::new('gigi', 'Zucon')
+        );
+
+        $approver = User::new('toni', 'Folpet');
+        $chocolate->approve($approver);
     }
 }
