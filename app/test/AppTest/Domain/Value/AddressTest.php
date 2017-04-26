630ce005f8d800586dda94f28b3129ae03d0859d
Marco Perone <m.perone@mvlabs.it>
2017-04-26 08:07:53 +0200

    test create address from values

--- app/test/AppTest/Domain/Value/AddressTest.php
+++ app/test/AppTest/Domain/Value/AddressTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AppTest\Domain\Value;
 
 use App\Domain\Value\Address;
+use App\Domain\Value\Country;
 use PHPUnit\Framework\TestCase;
 
 final class AddressTest extends TestCase
@@ -34,5 +35,32 @@ final class AddressTest extends TestCase
         self::assertSame($city, $address->city());
         self::assertSame($region, $address->region());
         self::assertSame(strtoupper($country), $address->countryCode()->getValue());
+    }
+
+    public function testFromStreetNumberZipCodeCityRegionCountry()
+    {
+        $street = 'via Daqui';
+        $number = '1A';
+        $zipCode = 'AB123';
+        $city = 'Treviso';
+        $region = 'TV';
+        $country = Country::fromStringCode('it');
+
+        $address = Address::fromStreetNumberZipCodeCityRegionCountry(
+            $street,
+            $number,
+            $zipCode,
+            $city,
+            $region,
+            $country
+        );
+
+        self::assertInstanceOf(Address::class, $address);
+        self::assertSame($street, $address->street());
+        self::assertSame($number, $address->streetNumber());
+        self::assertSame($zipCode, $address->zipCode());
+        self::assertSame($city, $address->city());
+        self::assertSame($region, $address->region());
+        self::assertSame($country->code(), $address->countryCode());
     }
 }
