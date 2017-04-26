25c3c4fe16a21d35005886aace0db6a2b6950701
Marco Perone <m.perone@mvlabs.it>
2017-04-26 07:52:51 +0200

    test create new user

--- app/test/AppTest/Domain/Entity/UserTest.php
+++ app/test/AppTest/Domain/Entity/UserTest.php
@@ -30,4 +30,17 @@ final class UserTest extends TestCase
         self::assertSame($surname, $user->surname());
         self::assertSame($isAdmin, $user->isAdministrator());
     }
+
+    public function testNew()
+    {
+        $name = 'Gigi';
+        $surname = 'Zucon';
+
+        $user = User::new($name, $surname);
+
+        self::assertInstanceOf(UserId::class, $user->id());
+        self::assertSame($name, $user->name());
+        self::assertSame($surname, $user->surname());
+        self::assertFalse($user->isAdministrator());
+    }
 }
