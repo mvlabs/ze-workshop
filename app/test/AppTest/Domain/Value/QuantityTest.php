8c8f3110fe7e113f5d2e881f0f8f6f1ba540977b
Marco Perone <m.perone@mvlabs.it>
2017-04-27 10:10:38 +0200

    test negative quantity

--- app/test/AppTest/Domain/Value/QuantityTest.php
+++ app/test/AppTest/Domain/Value/QuantityTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AppTest\Domain\Value;
 
+use App\Domain\Value\Exception\NegativeQuantityException;
 use App\Domain\Value\Quantity;
 use PHPUnit\Framework\TestCase;
 
@@ -16,5 +17,13 @@ final class QuantityTest extends TestCase
         $quantity = Quantity::grams($int);
 
         self::assertSame($int, $quantity->toInt());
+    }
+
+    public function testNegativeQuantityFails(): void
+    {
+        $this->expectException(NegativeQuantityException::class);
+        $this->expectExceptionMessage(sprintf('A quantity must be non negative. Received %s', -1));
+
+        Quantity::grams(-1);
     }
 }
