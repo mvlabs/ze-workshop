257dc8ca45214f16cc3797f8bf2fde62746cc6a8
Marco Perone <m.perone@mvlabs.it>
2017-04-27 10:13:44 +0200

    test negative and over 100 percentages

--- app/test/AppTest/Domain/Value/PercentageTest.php
+++ app/test/AppTest/Domain/Value/PercentageTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AppTest\Domain\Value;
 
+use App\Domain\Value\Exception\InvalidPercentageException;
 use App\Domain\Value\Percentage;
 use PHPUnit\Framework\TestCase;
 
@@ -16,5 +17,31 @@ final class PercentageTest extends TestCase
         $percentage = Percentage::integer($int);
 
         self::assertSame($int, $percentage->toInt());
+    }
+
+    public function testNegativePercentageFails(): void
+    {
+        $percentage = -1;
+
+        $this->expectException(InvalidPercentageException::class);
+        $this->expectExceptionMessage(sprintf(
+            'A percentage can not be smaller than zero. Received %s',
+            $percentage
+        ));
+
+        Percentage::integer($percentage);
+    }
+
+    public function testPercentageOver100Fails(): void
+    {
+        $percentage = 101;
+
+        $this->expectException(InvalidPercentageException::class);
+        $this->expectExceptionMessage(sprintf(
+            'A percentage can not be bigger than one hundred. Received %s',
+            $percentage
+        ));
+
+        Percentage::integer($percentage);
     }
 }
