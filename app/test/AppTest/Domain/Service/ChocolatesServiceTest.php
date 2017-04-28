aac1e970076395a11898024a7bd8acdec8aff1e5
Marco Perone <m.perone@mvlabs.it>
2017-04-28 08:08:23 +0200

    test get chocolate by id

--- app/test/AppTest/Domain/Service/ChocolatesServiceTest.php
+++ app/test/AppTest/Domain/Service/ChocolatesServiceTest.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace AppTest\Domain\Service;
 
+use App\Domain\Entity\Chocolate;
+use App\Domain\Entity\User;
 use App\Domain\Service\ChocolatesService;
+use App\Domain\Service\Exception\ChocolateNotFoundException;
+use App\Domain\Value\Address;
+use App\Domain\Value\ChocolateId;
+use App\Domain\Value\Country;
+use App\Domain\Value\Percentage;
+use App\Domain\Value\Producer;
+use App\Domain\Value\Quantity;
+use App\Domain\Value\WrapperType;
 use App\Infrastructure\Repository\Chocolates;
 use Mockery;
 use PHPUnit\Framework\TestCase;
@@ -18,20 +28,59 @@ final class ChocolatesServiceTest extends TestCase
      */
     private $service;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->chocolates = Mockery::mock(Chocolates::class);
         $this->service = new ChocolatesService($this->chocolates);
     }
 
-    public function testAll()
+    public function testAll(): void
     {
         $this->chocolates->shouldReceive('all');
 
         $this->service->getAll();
     }
 
-    protected function assertPostConditions()
+    public function testGetExistingChocolate(): void
+    {
+        $chocolateId = ChocolateId::new();
+        $chocolate = Chocolate::submit(
+            $chocolateId,
+            Producer::fromNameAndAddress(
+                'producer',
+                Address::fromStreetNumberZipCodeCityRegionCountry(
+                    'via Diqua',
+                    '1A',
+                    'AB123',
+                    'Treviso',
+                    'TV',
+                    Country::fromStringCode('IT')
+                )),
+            'description',
+            Percentage::integer(77),
+            WrapperType::get(WrapperType::BOX),
+            Quantity::grams(100),
+            User::new('gigi', 'Zucon')
+        );
+
+        $this->chocolates->shouldReceive('findById')->with($chocolateId)->andReturn($chocolate);
+
+        self::assertSame($chocolate, $this->service->getChocolate($chocolateId));
+    }
+
+    public function testGetNonExistingChocolate()
+    {
+        $chocolateId = ChocolateId::new();
+
+        $this->chocolates->shouldReceive('findById')->with($chocolateId)->andReturn(null);
+
+        $this->expectException(ChocolateNotFoundException::class);
+        $this->expectExceptionMessage(sprintf('No chocolate was found for id %s', (string) $chocolateId));
+
+        $this->service->getChocolate($chocolateId);
+    }
+
+    protected function assertPostConditions(): void
     {
         $container = Mockery::getContainer();
         if ($container != null) {
