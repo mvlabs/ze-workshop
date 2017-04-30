0e61b15e16d1f938213f08a4d8ebf7a47f210936
Marco Perone <m.perone@mvlabs.it>
2017-04-30 19:34:30 +0200

    test find by id with no result

--- app/test/AppTest/Infrastructure/Repository/SqlChocolatesTest.php
+++ app/test/AppTest/Infrastructure/Repository/SqlChocolatesTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AppTest\Infrastructure\Repository;
 
+use App\Domain\Value\ChocolateId;
 use App\Infrastructure\Repository\SqlChocolates;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\Statement;
@@ -55,7 +56,42 @@ final class SqlChocolatesTest extends TestCase
             'GROUP BY c.id, p.id'
         )->andReturn($statement);
 
-        $this->repository->all();
+        self::assertSame([], $this->repository->all());
+    }
+
+    public function testFindByIdWithNoResult(): void
+    {
+        $statement = Mockery::mock(Statement::class);
+        $statement->shouldReceive('fetch')->with(\PDO::FETCH_ASSOC)->andReturn(null);
+
+        $chocolateId = ChocolateId::new();
+
+        $this->connection->shouldReceive('executeQuery')->with(
+            'SELECT ' .
+            '   c.id, ' .
+            '   p.name, ' .
+            '   p.street, ' .
+            '   p.street_number, ' .
+            '   p.zip_code, ' .
+            '   p.city, ' .
+            '   p.region, ' .
+            '   p.country, ' .
+            '   c.description, ' .
+            '   c.cacao_percentage, ' .
+            '   c.wrapper_type, ' .
+            '   c.quantity, ' .
+            '   array_agg(ARRAY[ch.status, u.id, u.name, u.surname, cast(u.admin as varchar), cast(ch.date_time as varchar)]) AS history ' .
+            'FROM chocolates c ' .
+            'JOIN producers p ON p.id = c.producer_id ' .
+            'JOIN chocolates_history ch ON ch.chocolate_id = c.id ' .
+            'JOIN users u ON u.id = ch.user_id ' .
+            'WHERE c.id = :id',
+            [
+                'id' => (string) $chocolateId
+            ]
+        )->andReturn($statement);
+
+        self::assertNull($this->repository->findById($chocolateId));
     }
 
     protected function assertPostConditions(): void
