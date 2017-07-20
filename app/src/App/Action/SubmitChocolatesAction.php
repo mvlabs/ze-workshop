b6e3bf123eebc12f288092933836e13af5bf993f
Marco Perone <m.perone@mvlabs.it>
2017-07-20 10:42:01 +0200

    implement submit chocolates action

--- app/src/App/Action/SubmitChocolatesAction.php
+++ app/src/App/Action/SubmitChocolatesAction.php
@@ -4,28 +4,91 @@ declare(strict_types=1);
 
 namespace App\Action;
 
+use App\Domain\Entity\User;
+use App\Domain\Service\ChocolatesService;
+use App\Domain\Service\UsersService;
+use App\Domain\Value\Address;
+use App\Domain\Value\ChocolateId;
+use App\Domain\Value\Country;
+use App\Domain\Value\Exception\InvalidCountryCodeException;
+use App\Domain\Value\Exception\InvalidPercentageException;
+use App\Domain\Value\Exception\InvalidWrapperTypeException;
+use App\Domain\Value\Exception\NegativeQuantityException;
+use App\Domain\Value\Percentage;
+use App\Domain\Value\Producer;
+use App\Domain\Value\Quantity;
+use App\Domain\Value\WrapperType;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Middlewares\HttpAuthentication;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\HtmlResponse;
-use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Diactoros\Response\JsonResponse;
 
 final class SubmitChocolatesAction implements MiddlewareInterface
 {
     /**
-     * @var TemplateRendererInterface
+     * @var ChocolatesService
      */
-    private $template;
+    private $chocolatesService;
+
+    /**
+     * @var UsersService
+     */
+    private $usersService;
 
     public function __construct(
-        TemplateRendererInterface $template
+        ChocolatesService $chocolatesService,
+        UsersService $usersService
     ) {
-        $this->template = $template;
+        $this->chocolatesService = $chocolatesService;
+        $this->usersService = $usersService;
     }
 
     public function process(ServerRequestInterface $request, DelegateInterface $delegate): ResponseInterface
     {
-        return new HtmlResponse($this->template->render('app::submit'));
+        $data = $request->getParsedBody();
+
+        //try {
+            $chocolateId = ChocolateId::new();
+            $producer = Producer::fromNameAndAddress(
+                $data['producer_name'],
+                Address::fromStreetNumberZipCodeCityRegionCountry(
+                    $data['producer_street'],
+                    $data['producer_number'],
+                    $data['producer_zip_code'],
+                    $data['producer_city'],
+                    $data['producer_region'],
+                    Country::fromStringCode($data['producer_country'])
+                )
+            );
+            $description = $data['chocolate_description'];
+            $percentage = Percentage::integer((int) $data['chocolate_percentage']);
+            $wrapperType = WrapperType::get($data['chocolate_wrapper_type']);
+            $quantity = Quantity::grams((int) $data['chocolate_quantity']);
+            $user = $this->usersService->byUsername($request->getAttribute(HttpAuthentication::class));
+
+            $this->chocolatesService->submit(
+                $chocolateId,
+                $producer,
+                $description,
+                $percentage,
+                $wrapperType,
+                $quantity,
+                $user
+            );
+        /*} catch (InvalidCountryCodeException $e) {
+            // TODO
+        } catch (InvalidPercentageException $e) {
+            // TODO
+        } catch (InvalidWrapperTypeException $e) {
+            // TODO
+        } catch (NegativeQuantityException $e) {
+            // TODO
+        } catch (\Throwable $e) {
+            // TODO
+        }*/
+
+        return new JsonResponse([]);
     }
 }
---
+++ app/src/App/Container/Action/SubmitChocolatesActionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Container\Action;
+
+use App\Action\SubmitChocolatesAction;
+use App\Domain\Service\ChocolatesService;
+use App\Domain\Service\UsersService;
+use Interop\Container\ContainerInterface;
+
+final class SubmitChocolatesActionFactory
+{
+    public function __invoke(ContainerInterface $container, string $name): SubmitChocolatesAction
+    {
+        return new SubmitChocolatesAction(
+            $container->get(ChocolatesService::class),
+            $container->get(UsersService::class)
+        );
+    }
+}
