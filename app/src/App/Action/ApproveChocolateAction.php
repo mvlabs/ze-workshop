2961d9c85dabbda7e76b912c621530fa12307c32
Marco Perone <m.perone@mvlabs.it>
2017-08-04 15:28:57 +0200

    use renamed method name

--- app/src/App/Action/ApproveChocolateAction.php
+++ app/src/App/Action/ApproveChocolateAction.php
@@ -39,7 +39,7 @@ final class ApproveChocolateAction implements MiddlewareInterface
         $chocolateId = ChocolateId::fromString($request->getAttribute('id'));
         $chocolate = $this->chocolatesService->getChocolate($chocolateId);
 
-        $user = $this->usersService->byUsername($request->getAttribute(HttpAuthentication::class));
+        $user = $this->usersService->getByUsername($request->getAttribute(HttpAuthentication::class));
 
         $this->chocolatesService->approve($chocolate, $user);
 
