5579ec9fd8ac8b1684018752c136945f1c1475ec
Web Summer Camp 2017 <websummercamp@netgen.hr>
2017-08-10 13:01:26 +0200

    database seeds

---
+++ seeds/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, password, admin) VALUES
+('3a9ba7cf-6b36-4f80-b3e1-e91f8170dc0b', 'admin', 'password', true),
+('6afe33b6-118d-465e-8ec5-87c8baf36423', 'user', 'password', false);
