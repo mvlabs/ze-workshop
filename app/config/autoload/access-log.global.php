6b662b046f6c67c47b3f5966f7f6142de2c0deaa
Steve Maraspin <s.maraspin@mvlabs.it>
2017-08-12 14:29:08 +0200

    all (possible) logs are kept within their own folder

--- app/config/autoload/access-log.global.php
+++ app/config/autoload/access-log.global.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'access_log' => [
-        'path' => __DIR__ . '/../../data/access-log.txt'
+        'path' => __DIR__ . '/../../data/log/access-log.txt'
     ]
 ];
