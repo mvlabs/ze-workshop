8df821c83233fae79a111f3da32ee34f287aa634
Marco Perone <pasafama@gmail.com>
2017-08-24 22:58:43 +0200

    implementation of authorization middleware behat tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,17 +2,55 @@
 
 declare(strict_types=1);
 
+use App\Domain\Entity\User;
+use App\Domain\Service\Exception\UserNotFoundException;
+use App\Domain\Service\UsersServiceInterface;
+use App\Middleware\Authorization;
 use Behat\Behat\Context\Context;
-use Behat\Behat\Tester\Exception\PendingException;
+use PHPUnit\Framework\Assert;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Middleware\JwtAuthentication;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
+use Zend\Stratigility\Delegate\CallableDelegateDecorator;
 
 class FeatureContext implements Context
 {
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var UsersServiceInterface
+     */
+    private $usersService;
+
+    /**
+     * @var ResponseInterface
+     */
+    private $response;
+
+    /**
+     * @var \Exception|null
+     */
+    private $exception = null;
+
+    /** @BeforeScenario */
+    public function beforeScenario()
+    {
+        $this->usersService = \Mockery::mock(UsersServiceInterface::class);
+    }
+
     /**
      * @Given I am an administrator user
      */
     public function iAmAnAdministratorUser()
     {
-        throw new PendingException();
+        $this->user = User::newAdministrator('gigi', 'zucon');
+
+        $this->usersService->shouldReceive('getByUsername')->with('gigi')->andReturn($this->user);
     }
 
     /**
@@ -20,7 +58,30 @@ class FeatureContext implements Context
      */
     public function iTryToAccessAdministrationFunctionalities()
     {
-        throw new PendingException();
+        $authorizationMiddleware = new Authorization($this->usersService);
+
+        $request = (new ServerRequest())->withAttribute(
+            JwtAuthentication::class,
+            json_decode(json_encode([
+                'data' => [
+                    'username' => $this->user->username()
+                ]
+            ]))
+        );
+
+        try {
+            $this->response = $authorizationMiddleware->process(
+                $request,
+                new CallableDelegateDecorator(
+                    function (ServerRequestInterface $request, ResponseInterface $response) {
+                        return $response;
+                    },
+                    new Response()
+                )
+            );
+        } catch (\Exception $e) {
+            $this->exception = $e;
+        }
     }
 
     /**
@@ -28,7 +89,7 @@ class FeatureContext implements Context
      */
     public function iAmGrantedAccess()
     {
-        throw new PendingException();
+        Assert::assertSame(200, $this->response->getStatusCode());
     }
 
     /**
@@ -36,7 +97,9 @@ class FeatureContext implements Context
      */
     public function iAmANonAdministratorUser()
     {
-        throw new PendingException();
+        $this->user = User::new('gigi', 'zucon');
+
+        $this->usersService->shouldReceive('getByUsername')->with('gigi')->andReturn($this->user);
     }
 
     /**
@@ -44,7 +107,10 @@ class FeatureContext implements Context
      */
     public function iAmDeniedAccess()
     {
-        throw new PendingException();
+        Assert::assertTrue(
+            $this->exception instanceof UserNotFoundException ||
+            403 === $this->response->getStatusCode()
+        );
     }
 
     /**
@@ -52,7 +118,17 @@ class FeatureContext implements Context
      */
     public function iAmAUnknownUser()
     {
-        throw new PendingException();
+        $this->user = User::new('gigi', 'zucon');
+
+        $this->usersService
+            ->shouldReceive('getByUsername')
+            ->with('gigi')
+            ->andThrow(UserNotFoundException::fromUsername('gigi'));
     }
 
+    /** @AfterScenario */
+    public function afterScenario()
+    {
+        Mockery::close();
+    }
 }
